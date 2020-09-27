ceb85b5f75e91a893a11d651c650073075536a2d
insidious777 <777insidious777@gmail.com>
2020-09-27 13:58:30 +0300

    working receving messages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React,{useEffect, useState} from 'react';
+import React from 'react';
+import firebase, { firestore } from 'firebase/app'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
 import './App.css';
-import axios from 'axios';
+import 'firebase/firestore'
+import 'firebase/auth'
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDgTbRJvqFZnSYD4vGrFAQG4WIWo4ETKSg",
+    authDomain: "chat-dc70f.firebaseapp.com",
+    databaseURL: "https://chat-dc70f.firebaseio.com",
+    projectId: "chat-dc70f",
+    storageBucket: "chat-dc70f.appspot.com",
+    messagingSenderId: "665289931996",
+    appId: "1:665289931996:web:d6af2de6b5436050e4c819",
+    measurementId: "G-PKS9C2GQK2"
+})
+const auth = firebase.auth();
 function App() {
-  const [weatherData,setWeatherData]=useState(null);
-  useEffect(()=>{
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Ternopil&units=metric&appid=427f6d4f0d2bfadcc956f6ce714b2d52`)
-        .then(res => {
-          const info = res.data;
-          console.log(info);
-          setWeatherData(info);
-        })
-  },[]);
+const firestore = firebase.firestore();
+const [user] = useAuthState(auth)
   return (
     <div>
-      <label htmlFor="city">Enter your city</label>
-      <input name="city" type="text"></input>
-  {
-    weatherData?
-    <h1>{weatherData.name}</h1>:
-    <h1>Loading</h1>
-  }
-  </div>
+      {user?<div><ChatRoom/><SignOut/></div>:<SignIn/>}
+    </div>
   );
 }
 
+
+
+
+function SignIn(){
+  const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+  }
+  return(
+      <button onClick={signInWithGoogle}>SignIn</button>
+  )
+}
+
+
+
+
+
+function ChatRoom(){
+  const messageRef = firestore().collection('messages');
+  const query = messageRef.orderBy('createdAt').limit(25);
+const [messages] = useCollectionData(query, {idField:'id'});
+console.log(messages);
+  return(
+    <div>
+        <h1>ChatRoom</h1>
+        <div>
+        {messages?console.log(messages):null}
+        {messages && messages.map(msg=><ChatMessage key={msg.id} message={msg}/>)}
+        </div>
+    </div>
+  )
+}
+
+function ChatMessage(props){
+const {text, uid} = props.message;
+const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+return(
+<p>{text}:{messageClass}</p>
+)
+
+}
+
+
+function SignOut(){
+  return auth.currentUser && (
+  <button onClick={()=>auth.signOut()}>SignOut</button>
+  )
+}
 export default App;
